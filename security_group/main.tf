1a28c6b734c4ace93c107b8722a0ccfbfa118520
natachikhinashvili <natanatuka33@gmail.com>
2024-06-29 22:57:45 +0400

    feat: cidr range

--- security_group/main.tf
+++ security_group/main.tf
@@ -25,13 +25,13 @@ resource "aws_security_group" "rds" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
