8e140f6b735be7d369a6d5b1602c694b0f518809
natachikhinashvili <natanatuka33@gmail.com>
2024-07-01 15:25:50 +0400

    feat: ecs conf network

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "ecs_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+}
+
 resource "aws_iam_role" "ecs_instance_role" {
   name = "ecs_instance_role"
 
@@ -12,119 +22,135 @@ resource "aws_iam_role" "ecs_instance_role" {
     }]
   })
 }
-
+resource "aws_iam_role_policy_attachment" "ecs_attach" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
 resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
   role       = aws_iam_role.ecs_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
-
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "ecs_instance_profile"
   role = aws_iam_role.ecs_instance_role.name
 }
 
-data "aws_ssm_parameter" "ecs_node_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
-}
 
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-cluster"
 }
 
-resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
-  name                = "asg"
-  vpc_zone_identifier = var.privatesubnet
+resource "aws_autoscaling_group" "ecs_asg" {
+ vpc_zone_identifier = var.privatesubnet
+ desired_capacity    = 1
+ max_size            = 1
+ min_size            = 1
 
-  desired_capacity          = 1
-  min_size                  = 1
-  max_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "EC2"
-
-  launch_template {
-    id = aws_launch_template.engine.id
-    version = aws_launch_template.engine.latest_version
-  }
+ launch_template {
+   id      = aws_launch_template.ecs_lt.id
+   version = "$Latest"
+ }
 }
+resource "aws_launch_template" "ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = data.aws_ami.ecs_ami.image_id
+ instance_type = "t3.micro"
 
-resource "aws_launch_template" "engine" {
-  name          = "alma"
-  image_id      = data.aws_ssm_parameter.ecs_node_ami.value
-  instance_type = "t3.medium"
-  user_data     = base64encode("#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config")
-  
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ecs_instance_profile.name
-  }
+ vpc_security_group_ids = [var.security_group_ids]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
 
-  network_interfaces {
-    associate_public_ip_address = false
-    subnet_id                   = var.privatesubnet[0]
-    security_groups             = [var.security_group_ids]
-  }
+ block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 30
+     volume_type = "gp2"
+   }
+ }
+ user_data =  base64encode("#!/bin/bash\n echo ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config")
 }
-
-resource "aws_ecs_capacity_provider" "provider" {
-  name = "provider"
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "test1"
 
   auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.failure_analysis_ecs_asg.arn
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
     managed_scaling {
-      status                    = "ENABLED"
-      target_capacity           = 100
+      maximum_scaling_step_size = 1000
       minimum_scaling_step_size = 1
-      maximum_scaling_step_size = 10
+      status                    = "ENABLED"
+      target_capacity           = 3
     }
   }
 }
 
 resource "aws_ecs_cluster_capacity_providers" "ecs_cluster_capacity_providers" {
   cluster_name       = aws_ecs_cluster.ecs_cluster.name
-  capacity_providers = [aws_ecs_capacity_provider.provider.name]
+  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
 }
-
 
 resource "aws_ecs_task_definition" "task_definition" {
   family = "worker"
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2"]
-  memory      = 2048
-  cpu       = 1024
+  task_role_arn = "arn:aws:iam::850286438394:role/ecsTaskExecutionRole"
+  execution_role_arn = "arn:aws:iam::850286438394:role/ecsTaskExecutionRole"
+  cpu       = 256
+  runtime_platform {
+   operating_system_family = "LINUX"
+   cpu_architecture        = "X86_64"
+ }
 
   container_definitions = jsonencode([
     {
       essential   = true
       name        = "worker"
+      cpu       = 256
+      memory    = 512
       image       = "${var.repository_url}:latest"
       portMappings = [
         {
           containerPort = 3000
           hostPort      = 3000
+          protocol      = "tcp"
         }
       ]
     }
   ])
 }
 
-resource "aws_ecs_service" "worker" {
-  name            = "worker"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.task_definition.arn 
-  desired_count   = 1
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.task_definition.arn 
+ desired_count   = 2
 
-  capacity_provider_strategy {
-    capacity_provider = aws_ecs_capacity_provider.provider.name
-    weight            = 1
-  }
+ network_configuration {
+   subnets         = var.privatesubnet 
+   security_groups = [var.security_group_ids]
+ }
 
-  network_configuration {
-    security_groups = [var.security_group_ids]
-    subnets         = var.privatesubnet 
-  }
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
 
-  load_balancer {
-    target_group_arn = var.aws_lb_target_group_arn
-    container_name   = "worker"
-    container_port   = 3000
-  }
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = var.aws_lb_target_group_arn
+   container_name   = "worker"
+   container_port   = 3000
+ }
+
+ depends_on = [aws_autoscaling_group.ecs_asg]
 }
--- security_group/main.tf
+++ security_group/main.tf
@@ -19,14 +19,14 @@ resource "aws_security_group" "load_balancer" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
