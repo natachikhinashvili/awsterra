3d8f394d46bb66eec4b303679e2a8fcbe314be44
natachikhinashvili <natanatuka33@gmail.com>
2024-07-04 18:41:29 +0400

    feat: ip cidr

--- security_group/main.tf
+++ security_group/main.tf
@@ -22,14 +22,14 @@ resource "aws_security_group" "load_balancer" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
 
