fdf5dcc3779e494e1a4c57f00d7b88daf6ab6141
natachikhinashvili <natanatuka33@gmail.com>
2024-07-05 12:31:38 +0400

    feat: instance profile name

--- ecs/main.tf
+++ ecs/main.tf
@@ -49,7 +49,7 @@ resource "aws_launch_template" "ecs_lt" {
   instance_type = "t3.micro"
 
   iam_instance_profile {
-    name = "ecsInstanceRole"
+    name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
   block_device_mappings {
