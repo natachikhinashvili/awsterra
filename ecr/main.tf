1325bb8610cdbb5c87805b457f9f8f9f4c947e87
natachikhinashvili <natanatuka33@gmail.com>
2024-07-08 12:19:01 +0400

    feat: dockerfile location

--- ecr/main.tf
+++ ecr/main.tf
@@ -6,7 +6,7 @@ resource "null_resource" "docker_build_and_push" {
   provisioner "local-exec" {
     command = <<EOF
       $(aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${aws_ecr_repository.nats_repo.repository_url})
-      sudo docker build -t ${aws_ecr_repository.nats_repo.repository_url}:latest .
+      sudo docker build -t ${aws_ecr_repository.nats_repo.repository_url}:latest ./coolweb/
       sudo docker push ${aws_ecr_repository.nats_repo.repository_url}:latest
     EOF
   }
