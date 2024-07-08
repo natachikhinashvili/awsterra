9cd18bab4c73881ad35e80c44bf063a813fad3a2
natachikhinashvili <natanatuka33@gmail.com>
2024-07-08 17:12:41 +0400

    feat: damaviwyda charchraa

--- coolweb/index.js
+++ coolweb/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, zuraa!');
+  res.send('Hello, chavicheri!');
 });
 
 app.listen(80, () => {
