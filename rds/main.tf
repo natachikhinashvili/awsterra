f5ced3e5060e8bf0708c920c8bb517249da97005
nata tchikhinashvili <86610861+natachikhinashvili@users.noreply.github.com>
2024-07-11 17:39:40 +0400

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "main" {
   instance_class         = "db.t3.micro"
   db_name                = var.db_name
   username               = var.username
-  password               = var.password
+  password               = random_password.password.result
   parameter_group_name   = "default.mysql8.0"
   db_subnet_group_name   = var.subnetgroup
   vpc_security_group_ids = var.securitygroup
@@ -19,4 +19,22 @@ output "db_instance_endpoint" {
 
 output "db_instance_id" {
   value = aws_db_instance.main.id
+}
+
+resource "random_password" "password" {
+  length  = 16
+  special = true
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "aws_secretsmanager_secret" "password" {
+  name = "core-${var.environment}-password-${random_id.id.hex}"
+}
+
+resource "aws_secretsmanager_secret_version" "password" {
+  secret_id     = aws_secretsmanager_secret.password.id
+  secret_string = random_password.password.result
 }
